3046feaf657e7cb13c919e90513163df1dabdb57
rory jackson <roryjamesjackson@hotmail.com>
2023-09-29 10:18:35 -0400

    text adjustments

--- src/components/Course6.js
+++ src/components/Course6.js
@@ -5,7 +5,7 @@ const Course6 = ({ heading }) => (
   <div>
     <h1>{heading}</h1>
     {/* Content for Page 1 */}
-    <p>She gave up all that was dear to her; her friends, family and career as a detective to move to the US. Cameron must taste very good to give up all that, so we've tried to replicate this for your final course of the evening...</p>
+    <p>She gave up all that was dear to her; friends, family and career as a detective to move to the US. Cameron must have a very enticing, unique flavour to make her do that. So we've tried to replicate it for your final course of the evening...</p>
     <div className='image-container'>
       <img className='image1' src="https://res.cloudinary.com/dfipoufmj/image/upload/v1695932717/PXL_20230926_015740236.PORTRAIT_hj8q0h.jpg" alt="hjdosh"/>
     </div>
